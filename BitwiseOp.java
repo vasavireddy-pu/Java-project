47b9c5fd7850987b56bdf98f9c48b471c172b548
Vasavi Reddy Pusapati <vasavireddypusapati@gmail.com>
2025-06-21 23:48:54 +0530

    Add files via upload

---
+++ BitwiseOp.java
@@ -0,0 +1,9 @@
+public class BitwiseOp {
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=12;
+        System.out.println(num1&num2);
+        System.out.println(num1|num2);
+        System.out.println(num1^num2);
+    }
+}
---
+++ LogicalOperator.java
@@ -0,0 +1,11 @@
+public class LogicalOperator {
+    public static void main(String[] args) {
+        boolean val1 = true;
+        boolean val2 = false;
+        System.out.println(val1&&val2);
+        int num1=12;
+        int num2=10;
+        System.out.println((num1>num2)||(num1<num2));
+        System.out.println(!(num1>num2));
+    }
+}

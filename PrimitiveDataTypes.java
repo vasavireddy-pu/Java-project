36dbece61b838065653ec7febd12a59969e3af48
Vasavi Reddy Pusapati <vasavireddypusapati@gmail.com>
2025-06-21 16:57:25 +0530

    Add files via upload

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,23 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] agrs) {
+        //Declare and initialize variables of primitive data types
+        byte byteValue = 127;
+        short shortValue = 32767;
+        int intValue = 2147483647;
+        long longValue = 9223372036854775807L;
+        float floatValue = 3.14f;
+        double doubleValue = 2.71;
+        boolean boolValue = true;
+        char charValue = 'A';
+
+        //Print the values of the variables
+        System.out.println("byteValue: " + byteValue);
+        System.out.println("shortValue: " +shortValue);
+        System.out.println("intValue: " + intValue);
+        System.out.println("longValue: " + longValue);
+        System.out.println("floatValue: " + floatValue);
+        System.out.println("doubleValue: " + doubleValue);
+        System.out.println("boolValue: " + boolValue);
+        System.out.println("charValue: " + charValue);
+    }
+}
